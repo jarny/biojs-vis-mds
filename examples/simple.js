9691eb872f86b199bc7a92921ec200ce546f4e11
Yoshua Wakeham <wakeham.y@wehi.edu.au>
2016-11-25 17:43:50 +1100

    WIP - begin work on data formatting and plotly implementation of scatterplot

--- examples/simple.js
+++ examples/simple.js
@@ -1,3 +1,15 @@
 // if you don't specify a html file, the sniper will generate a div with id "rootDiv"
-var app = require("biojs-vis-mds");
-var instance = new app({el: rootDiv, text: 'biojs'});
+var mdsvis = require("biojs-vis-mds");
+var points = [ [0,1,2,3],
+               [3,2,4,1],
+               [2,3,1,2],
+               [0,0,3,7]];
+var metadata = [ {celltype: 'B2', tissue: 'BM'},
+                 {celltype: 'Mac', tissue: 'BM'},
+                 {celltype: 'B2', tissue: 'LN'},
+                 {celltype: 'Mac', tissue: 'LN'} ];
+mdsvis.run({
+  el: rootDiv,
+  coords: points,
+  metadata: metadata
+});
